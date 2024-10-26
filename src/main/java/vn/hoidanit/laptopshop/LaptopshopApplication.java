00cf687c27fd840a6ddecef63178fdf15de4596e
Ahryxx <phvanthanh06@gmail.com>
2024-10-26 13:50:15 +0700

    Video #46

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -17,5 +17,4 @@ public class LaptopshopApplication {
 			System.out.println(s);
 		}
 	}
-
 }
--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,16 +9,7 @@ import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello From Controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -27,8 +18,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "ahryxx.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }

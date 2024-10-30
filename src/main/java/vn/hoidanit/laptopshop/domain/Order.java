1cda75366407e56713976f054751a07947ec4457
Ahryxx <phvanthanh06@gmail.com>
2024-10-30 10:56:12 +0700

    Video #79

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +16,9 @@ public class Order {
     private long id;
 
     // user id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     private double totalPrice;
 
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,13 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users");
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +26,14 @@ public class User {
 
     private String avatar;
 
-    // roleid
+    // roleId
+    // User Many to One Role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     // Getter-Setter
 
